f3a3ab398076f3f5902863d8e866900031928601
Castillo14 <castillomaryjoy14@gmail.com>
2020-12-15 18:52:34 +0800

    first

---
+++ doctors_appointment/admin/db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+
+$conn= new mysqli('localhost','urafnghd_root','reniertrenuela9','urafnghd_moral')or die("Could not connect to mysql".mysqli_error($con));
---
+++ doctors_appointment/home.php
@@ -0,0 +1,331 @@
+<?php 
+include 'admin/db_connect.php'; 
+?>
+<!DOCTYPE html> 
+<html lang="en">
+    
+<!-- doccure/  30 Nov 2019 04:11:34 GMT -->
+<head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Doccure</title>
+        
+        <!-- Favicons -->
+        <link type="image/x-icon" href="assets/img/favicon.png" rel="icon">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        
+        <!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+        <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+        
+        <!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+            <script src="assets/js/html5shiv.min.js"></script>
+            <script src="assets/js/respond.min.js"></script>
+        <![endif]-->
+    
+    </head>
+<style>
+#portfolio .img-fluid{
+    width:100%
+}
+@media (min-width: 768px) {
+.carousel-multi-item-2 .col-md-3 {
+float: left;
+width: 25%;
+max-width: 100%; } }
+
+.carousel-multi-item-2 .card img {
+border-radius: 2px; }
+</style>
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4 page-title">
+                    	<h3 class="text-white">Welcome to <?php echo $_SESSION['setting_name']; ?></h3>
+                        <hr class="divider my-4" />
+                        <a class="btn btn-primary btn-xl js-scroll-trigger" href="index.php?page=doctors">Book an Appointment</a>
+
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+	
+   <section class="section section-features">
+                <div class="container-fluid">
+                   <div class="row">
+                        <div class="col-md-5 features-img">
+                            <img src="img/2.jpeg" class="img-fluid" alt="Feature">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="section-header">    
+                                <h2 class="mt-2">Availabe Features in Our Hospital</h2>
+                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+                            </div>  
+                            <div class="features-slider slider">
+                                <!-- Slider Item -->
+                                <div class="feature-item text-center">
+                                    <img src="assets/img/features/feature-01.jpg" class="img-fluid" alt="Feature">
+                                    <p>Patient Ward</p>
+                                </div>
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="feature-item text-center">
+                                    <img src="assets/img/features/feature-02.jpg" class="img-fluid" alt="Feature">
+                                    <p>Test Room</p>
+                                </div>
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="feature-item text-center">
+                                    <img src="assets/img/features/feature-03.jpg" class="img-fluid" alt="Feature">
+                                    <p>ICU</p>
+                                </div>
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="feature-item text-center">
+                                    <img src="assets/img/features/feature-04.jpg" class="img-fluid" alt="Feature">
+                                    <p>Laboratory</p>
+                                </div>
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="feature-item text-center">
+                                    <img src="assets/img/features/feature-05.jpg" class="img-fluid" alt="Feature">
+                                    <p>Operation</p>
+                                </div>
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="feature-item text-center">
+                                    <img src="assets/img/features/feature-06.jpg" class="img-fluid" alt="Feature">
+                                    <p>Medical</p>
+                                </div>
+                                <!-- /Slider Item -->
+                            </div>
+                        </div>
+                   </div>
+                </div>
+            </section>      
+
+<!--Carousel Wrapper-->
+<div id="multi-item-example" class="carousel slide carousel-multi-item carousel-multi-item-2" data-ride="carousel">
+
+  <!--Controls-->
+  <div class="controls-top">
+    <a class="black-text" href="#multi-item-example" data-slide="prev"><i class="fas fa-angle-left fa-3x pr-3"></i></a>
+    <a class="black-text" href="#multi-item-example" data-slide="next"><i class="fas fa-angle-right fa-3x pl-3"></i></a>
+  </div>
+  <!--/.Controls-->
+
+  <!--Slides-->
+  <div class="carousel-inner" role="listbox">
+
+    <!--First slide-->
+    <div class="carousel-item active">
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/2.jpeg"
+            alt="Card image cap">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/3.jpeg"
+            alt="Card image cap">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/4.jpeg"
+            alt="Card image cap">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/6.jpeg"
+            alt="Card image cap">
+        </div>
+      </div>
+
+    </div>
+    <!--/.First slide-->
+
+    <!--Second slide-->
+    <div class="carousel-item">
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/features/feature-01.jpg"
+            alt="Card image cap">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/features/feature-02.jpg"
+            alt="Card image cap">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/features/feature-03.jpg"
+            alt="Card image cap">
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="img/features/feature-04.jpg"
+            alt="Card image cap">
+        </div>
+      </div>
+
+    </div>
+    <!--/.Second slide-->
+
+   
+
+  </div>
+  <!--/.Slides-->
+
+</div>
+<!--/.Carousel Wrapper-->
+<!-- Clinic and Specialities -->
+            <section class="section section-specialities">
+                <div class="container-fluid">
+                    <div class="section-header text-center">
+                        <h2>Clinic and Specialities</h2>
+                        <p class="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-md-9">
+                            <!-- Slider -->
+                            <div class="specialities-slider slider">
+                            
+                                <!-- Slider Item -->
+                                <div class="speicality-item text-center">
+                                    <div class="speicality-img">
+                                        <img src="assets/img/specialities/specialities-01.png" class="img-fluid" alt="Speciality">
+                                        <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                    </div>
+                                    <p>Urology</p>
+                                </div>  
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="speicality-item text-center">
+                                    <div class="speicality-img">
+                                        <img src="assets/img/specialities/specialities-02.png" class="img-fluid" alt="Speciality">
+                                        <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                    </div>
+                                    <p>Neurology</p>    
+                                </div>                          
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="speicality-item text-center">
+                                    <div class="speicality-img">
+                                        <img src="assets/img/specialities/specialities-03.png" class="img-fluid" alt="Speciality">
+                                        <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                    </div>  
+                                    <p>Orthopedic</p>   
+                                </div>                          
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="speicality-item text-center">
+                                    <div class="speicality-img">
+                                        <img src="assets/img/specialities/specialities-04.png" class="img-fluid" alt="Speciality">
+                                        <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                    </div>  
+                                    <p>Cardiologist</p> 
+                                </div>                          
+                                <!-- /Slider Item -->
+                                
+                                <!-- Slider Item -->
+                                <div class="speicality-item text-center">
+                                    <div class="speicality-img">
+                                        <img src="assets/img/specialities/specialities-05.png" class="img-fluid" alt="Speciality">
+                                        <span><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                    </div>  
+                                    <p>Dentist</p>
+                                </div>                          
+                                <!-- /Slider Item -->
+                                
+                            </div>
+                            <!-- /Slider -->
+                            
+                        </div>
+                    </div>
+                </div>   
+            </section>   
+            <!-- Clinic and Specialities -->
+            <!-- Availabe Features -->
+    <div id="portfolio" class="container">
+            <div class="container-fluid p-0">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                    <h2 class="mb-4">Medical Specialties</h2>
+                    <hr class="divider">
+
+                    </div>
+                </div>
+                <div class="row no-gutters">
+                    <?php
+                    $cats = $conn->query("SELECT * FROM medical_specialty order by id asc");
+                                while($row=$cats->fetch_assoc()):
+                    ?>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="index.php?page=doctors&sid=<?php echo $row['id'] ?>">
+                            <img class="img-fluid" src="assets/img/<?php echo $row['img_path'] ?>" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name"><?php echo $row['name'] ?></div>
+                                <div class="project-category text-white">Find Doctor</div>
+                            </div>
+                        </a>
+                    </div>
+                    <?php endwhile; ?>
+                    
+                </div>
+            </div>
+        </div>
+
+    <script>
+        
+        $('.view_prod').click(function(){
+            uni_modal_right('Product','view_prod.php?id='+$(this).attr('data-id'))
+        })
+    </script>
+    <script src="assets/js/jquery.min.js"></script>
+        
+        <!-- Bootstrap Core JS -->
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        
+        <!-- Slick JS -->
+        <script src="assets/js/slick.js"></script>
+        
+        <!-- Custom JS -->
+        <script src="assets/js/script.js"></script>
+        
+    </body>
+
+
+</html>
+	
